26e786fc4ce69dc802cbf2754206a8f3e0962e16
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-05 17:18:57 +0530

    Refactor grab_files.py to accept multiple source directories and update meson.build to remove custom target for files.txt

--- grab_files.py
+++ grab_files.py
@@ -3,18 +3,18 @@
 from os import walk
 import sys
 
-if len(sys.argv) != 2:
-    rootDir = '.'
-else:
-    rootDir = sys.argv[1]
+if len(sys.argv) < 2:
+    print("Usage: grab_files.py <dir1> [<dir2> ...]")
+    sys.exit(1)
+srcDirs = sys.argv[1:]
 
-srcFiles = []
-for (dirpath, dirnames, filenames) in walk(f'{rootDir}/src'):
-    for filename in filenames:
-        if filename.endswith('.c'):
-            dirpath = dirpath.replace(f"{rootDir}/", "")
-            srcFiles.append(f"{dirpath}/{filename}")
+files = []
 
-with open(f'{rootDir}/files.txt', 'w') as f:
-    for file in srcFiles:
-        f.write(f"{file}\n")
+for srcDir in srcDirs:
+    for (dirpath, dirnames, filenames) in walk(f'{srcDir}'):
+        for filename in filenames:
+            if filename.endswith('.c'):
+                files.append(f"{dirpath}/{filename}")
+
+for cFile in files:
+    print(cFile)
