b1e999e873268ecb7891bd5a2ea83bbab11a33fb
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-06 00:26:34 +0530

    Update meson.build to include unity library and modify test setup in some_test.c

--- test/some_test.c
+++ test/some_test.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include "calculator.h"
+#include "unity/unity.h"
+
+void setUp(void){
+}
+
+void tearDown(void){
+}
 
 int main(){
-    if(add(2, 3) != 5) {
-        printf("Test failed: add(2, 3) should be 5\n");
-        return 1;
-    } else {
-        printf("Test passed: add(2, 3) = %d\n", add(2, 3));
-        return 0;
-    }
+    UNITY_BEGIN();
+    return UNITY_END();
+
+    // if(add(2, 3) != 5) {
+    //     printf("Test failed: add(2, 3) should be 5\n");
+    //     return 1;
+    // } else {
+    //     printf("Test passed: add(2, 3) = %d\n", add(2, 3));
+    //     return 0;
+    // }
 }
