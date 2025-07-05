83eb121a0f38d7ac2f9ec10dc88240fc2f28c247
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-06 00:15:20 +0530

    Add calculation examples to main function in main.c

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include "calculator.h"
 
 int main() {
     printf("Hello, World! from meson\n");
+    // Perform some calculations
+    printf("2 + 3 = %d\n", add(2, 3));
+    printf("5 - 2 = %d\n", subtract(5, 2));
+    printf("3 * 4 = %d\n", multiply(3, 4));
+    printf("8 / 2 = %d\n", divide(8, 2));
     return 0;
 }
