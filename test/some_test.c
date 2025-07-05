6a87b0f55d0e90d75f33758bd7d2ebd0b511fa9b
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-06 00:14:37 +0530

    Refactor meson.build for improved structure and update some_test.c to include functional tests

--- test/some_test.c
+++ test/some_test.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
+#include "calculator.h"
 
 int main(){
-    printf("running some test\n");
+    if(add(2, 3) != 5) {
+        printf("Test failed: add(2, 3) should be 5\n");
+        return 1;
+    } else {
+        printf("Test passed: add(2, 3) = %d\n", add(2, 3));
+        return 0;
+    }
 }
