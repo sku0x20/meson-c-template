8184578a60e12f128e15018139393b44b5c5cdf6
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-05 16:45:47 +0530

    Update grab_files.py and meson.build to use dynamic root directory for file generation

--- grab_files.py
+++ grab_files.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 from os import walk
+import sys
+
+if len(sys.argv) != 2:
+    rootDir = '.'
+else:
+    rootDir = sys.argv[1]
 
 srcFiles = []
-for (dirpath, dirnames, filenames) in walk('./src'):
+for (dirpath, dirnames, filenames) in walk(f'{rootDir}/src'):
     for filename in filenames:
-        if filename.endswith('.c'): 
+        if filename.endswith('.c'):
+            dirpath = dirpath.replace(f"{rootDir}/", "")
             srcFiles.append(f"{dirpath}/{filename}")
 
-with open('files.txt', 'w') as f:
+with open(f'{rootDir}/files.txt', 'w') as f:
     for file in srcFiles:
         f.write(f"{file}\n")
