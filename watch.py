2a709c760c461beed85c5db03d9f8a8cf40d5a70
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-08 12:33:23 +0530

    Update `watch.py` to touch `meson.build` on filesystem changes

--- watch.py
+++ watch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+from pathlib import Path
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 
@@ -9,15 +10,17 @@ if len(sys.argv) < 2:
     sys.exit(1)
 dirs = sys.argv[1:]
 
+buildFile = Path("meson.build")
+def touchBuild():
+    buildFile.touch()
 
 class CustomEventHandler(FileSystemEventHandler):
     def on_created(self, event: FileSystemEvent) -> None:
-        print(event)
+        touchBuild()
     def on_deleted(self, event: FileSystemEvent) -> None:
-        print(event)
+        touchBuild()
     def on_moved(self, event: FileSystemEvent) -> None:
-        print(event)
-
+        touchBuild()
 
 event_handler = CustomEventHandler()
 observer = Observer()
