fdf7570cb471f118cc2955470e31a7b8ce1e2829
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-08 12:27:07 +0530

    Add `watch.py` script to monitor filesystem changes using Watchdog library

---
+++ watch.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
+
+if len(sys.argv) < 2:
+    print("Usage: watch.py <dir1> [<dir2> ...]")
+    sys.exit(1)
+dirs = sys.argv[1:]
+
+
+class CustomEventHandler(FileSystemEventHandler):
+    def on_created(self, event: FileSystemEvent) -> None:
+        print(event)
+    def on_deleted(self, event: FileSystemEvent) -> None:
+        print(event)
+    def on_moved(self, event: FileSystemEvent) -> None:
+        print(event)
+
+
+event_handler = CustomEventHandler()
+observer = Observer()
+
+for dir in dirs:
+    observer.schedule(event_handler, dir, recursive=True)
+
+observer.start()
+
+try:
+    print("watching filesystem changes...")
+    while True:
+        input()
+except KeyboardInterrupt:
+    pass
+finally:
+    observer.stop()
+    observer.join()
